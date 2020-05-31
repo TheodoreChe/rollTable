0b9c0d1a772fbd7e7fa1710dc25c1ce0db198ffb
theodoreche <2226390@gmail.com>
2020-05-31 13:43:40 +0300

    move all files in subfolder

---
+++ rollTable/Styles/MainStyle.swift
@@ -0,0 +1,24 @@
+//
+//  MainStyle.swift
+//  Lines
+//
+//  Created by Федор Чернин on 30.05.2020.
+//  Copyright © 2020 Theodore Che. All rights reserved.
+//
+
+import UIKit
+
+public extension UIColor {
+    static var mainDarkBlue: UIColor {
+        return UIColor(red: 0.035, green: 0.145, blue: 0.537, alpha: 1)
+    }
+    static var mainBlue: UIColor {
+        return UIColor(red: 0, green: 0.412, blue: 1, alpha: 1)
+    }
+    static var mainDarkGreen: UIColor {
+        return UIColor(red: 0, green: 0.842, blue: 0.495, alpha: 1)
+    }
+    static var mainGreen: UIColor {
+        return UIColor(red: 0, green: 1, blue: 0.588, alpha: 1)
+    }
+}
